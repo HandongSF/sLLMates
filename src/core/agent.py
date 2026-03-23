585c1964bd3f19852e99a560216ab5ef9703d221
joshuaii3712 <joshuaii3712@gmail.com>
2026-03-23 21:00:46 +0900

    classifier branch to fusion branch

--- src/core/agent.py
+++ src/core/agent.py
@@ -265,6 +265,8 @@ class ChatAgent:
             return "classifier"
         elif state.get("branch_name") == "bio":
             return "bio"
+        elif state.get("branch_name") == "fusion":
+            return "fusion"
         else:
             return "default"
         
@@ -737,7 +739,6 @@ class ChatAgent:
             "final_answer": response
         }
 
-    # branch name: 
     # branch name: bio
 
     def bio_retrieve_bio_memory(self, state: State, top_k: int = 5, threshold: float = 1.0):
@@ -919,6 +920,231 @@ class ChatAgent:
         self.scheduler.start()
         print("[Bio Scheduler] 매일 자정 기한이 만료된 Bio memory가 자동 삭제됩니다.")
 
+    # branch name: fusion
+
+    def fusion_check_thinking(self, state: State):
+        if self.classifier_tokenizer == None or self.classifier_llm == None:
+            print("에러: classifier_tokenizer 또는 classifier_llm 없음")
+            sys.exit(1)
+
+        id2label = {0: "Non-thinking", 1: "Thinking"}
+
+        print(">>>query: " + state["query"].content)
+
+        raw_tokens = self.classifier_tokenizer.encode(state["query"].content, add_special_tokens=False)
+
+        if len(raw_tokens) > 510:
+            print(f"Query가 너무 길어 512 토큰에 맞게 자르기 시행 ({len(raw_tokens)} -> 510)")
+            raw_tokens = raw_tokens[:128] + raw_tokens[-382:]
+
+        input_ids = [self.classifier_tokenizer.cls_token_id] + raw_tokens + [self.classifier_tokenizer.sep_token_id]
+
+        inputs = {
+            "input_ids": torch.tensor([input_ids]).to(self.classifier_llm.device),
+            "attention_mask": torch.ones((1, len(input_ids))).to(self.classifier_llm.device)
+        }
+
+        with torch.no_grad():
+            outputs = self.classifier_llm(**inputs)
+    
+        logits = outputs.logits
+        probabilities = torch.nn.functional.softmax(logits, dim=-1)
+        predicted_class_id = torch.argmax(probabilities, dim=-1).item()
+
+        label_name = id2label.get(predicted_class_id, f"LABEL_{predicted_class_id}")
+
+        print(">>Label_name: " + label_name)
+
+        return {
+            "variables": state["variables"],
+            "system_prompt": state["system_prompt"],
+            "branch_name": label_name,
+            "messages": None,
+            "tools_result": None,
+            "query": state["query"],
+            "final_answer": None
+        }
+
+    def fusion_query_or_respond(self, state: State):
+        filled_system_prompt = state["system_prompt"].format(**state["variables"])
+
+        conversation_messages = [
+            message
+            for message in state["history"]
+            if message.type in ("human") or (message.type == "ai" and not message.tool_calls)
+        ]
+
+        trimmed_messages = self.trimmer.invoke([SystemMessage(filled_system_prompt)] + conversation_messages + [state["query"]])
+
+        openai_formatted_trimmed_messages = convert_to_openai_messages(trimmed_messages)
+
+        openai_formatted_tools = [convert_to_openai_tool(tool) for tool in self.tool_list]
+
+        #pprint(openai_formatted_trimmed_messages)
+
+        if self.formatter:            
+            full_prompt = self.formatter(
+                messages = openai_formatted_trimmed_messages,
+                tools = openai_formatted_tools,
+            ).prompt
+
+            if state["branch_name"] == "Non-thinking":
+                full_prompt += '<think>\n\n</think>\n\n'
+
+            print(full_prompt)
+            print('\n\n\n\n')
+
+            response_data = self.llm.create_completion(
+                prompt = full_prompt,
+                max_tokens = self.config["CHAT_MODEL_CONFIG"].get("max_tokens", 16),
+                temperature = self.config["CHAT_MODEL_CONFIG"].get("temperature", 0.8),
+                top_p = self.config["CHAT_MODEL_CONFIG"].get("top_p", 0.95),
+                min_p = self.config["CHAT_MODEL_CONFIG"].get("min_p", 0.05),
+                stop = self.config["CHAT_MODEL_CONFIG"].get("stop", []),
+                top_k = self.config["CHAT_MODEL_CONFIG"].get("top_k", 40),    
+            )
+
+            pprint(response_data)
+            print('\n\n\n\n')
+
+            text_output = response_data['choices'][0]['text'].strip()
+        else:
+            print("formatter = None")
+
+            response_data = self.llm.create_chat_completion(
+                messages = openai_formatted_trimmed_messages,
+                tools = openai_formatted_tools,
+                max_tokens = self.config["CHAT_MODEL_CONFIG"].get("max_tokens", 16),
+                temperature = self.config["CHAT_MODEL_CONFIG"].get("temperature", 0.8),
+                top_p = self.config["CHAT_MODEL_CONFIG"].get("top_p", 0.95),
+                min_p = self.config["CHAT_MODEL_CONFIG"].get("min_p", 0.05),
+                stop = self.config["CHAT_MODEL_CONFIG"].get("stop", []),
+                top_k = self.config["CHAT_MODEL_CONFIG"].get("top_k", 40),  
+            )
+
+            pprint(response_data)
+
+            text_output = response_data['choices'][0]['message']['content'].strip()
+        
+        response = parse_llm_output(text_output)
+
+        pprint(response)
+        print('\n\n\n\n')
+ 
+        if response.tool_calls:
+            return {
+                "variables": state["variables"],
+                "system_prompt": state["system_prompt"],
+                "branch_name": state["branch_name"],
+                "messages": [response],
+                "tools_result": None,
+                "query": state["query"],
+                "final_answer": None
+            }
+        
+        add_messages = [state["query"]] + [response]
+
+        return {
+            "variables": state["variables"],
+            "system_prompt": state["system_prompt"],
+            "history": add_messages,
+            "branch_name": state["branch_name"],
+            "messages": None,
+            "tools_result": None,
+            "query": state["query"],
+            "final_answer": response
+        }
+
+    def fusion_check_for_tools(self, state: State):
+        if state.get("messages"):
+            return "tools"
+        else:
+            return "no_tool"
+
+    def fusion_run_tools_and_pass_through_state(self, state: State):
+        tools_result = self.tools.invoke(state["messages"])
+
+        return {
+            "variables": state["variables"],
+            "system_prompt": state["system_prompt"],
+            "branch_name": state["branch_name"],
+            "messages": state["messages"],
+            "tools_result": tools_result,
+            "query": state["query"],
+            "final_answer": None
+        }
+
+    def fusion_generate(self, state: State):
+        filled_system_prompt = state["system_prompt"].format(**state["variables"])
+
+        conversation_messages = [
+            message
+            for message in state["history"]
+            if message.type in ("human") or (message.type == "ai" and not message.tool_calls)
+        ]
+
+        trimmed_messages = self.trimmer.invoke([SystemMessage(filled_system_prompt)] + conversation_messages + state["tools_result"] + [state["query"]])
+
+        openai_formatted_trimmed_messages = convert_to_openai_messages(trimmed_messages)
+
+        pprint(openai_formatted_trimmed_messages)
+
+        if self.formatter:
+            full_prompt = self.formatter(
+                messages = openai_formatted_trimmed_messages,
+            ).prompt
+
+            if state["branch_name"] == "Non-thinking":
+                full_prompt += '<think>\n\n</think>\n\n'
+
+            print(full_prompt)
+
+            response_data = self.llm.create_completion(
+                prompt = full_prompt,
+                max_tokens = self.config["CHAT_MODEL_CONFIG"].get("max_tokens", 16),
+                temperature = self.config["CHAT_MODEL_CONFIG"].get("temperature", 0.8),
+                top_p = self.config["CHAT_MODEL_CONFIG"].get("top_p", 0.95),
+                min_p = self.config["CHAT_MODEL_CONFIG"].get("min_p", 0.05),
+                stop = self.config["CHAT_MODEL_CONFIG"].get("stop", []),
+                top_k = self.config["CHAT_MODEL_CONFIG"].get("top_k", 40),    
+            )
+
+            pprint(response_data)
+
+            text_output = response_data['choices'][0]['text'].strip()
+        else:
+            print("formatter = None")
+
+            response_data = self.llm.create_chat_completion(
+                messages = openai_formatted_trimmed_messages,
+                max_tokens = self.config["CHAT_MODEL_CONFIG"].get("max_tokens", 16),
+                temperature = self.config["CHAT_MODEL_CONFIG"].get("temperature", 0.8),
+                top_p = self.config["CHAT_MODEL_CONFIG"].get("top_p", 0.95),
+                min_p = self.config["CHAT_MODEL_CONFIG"].get("min_p", 0.05),
+                stop = self.config["CHAT_MODEL_CONFIG"].get("stop", []),
+                top_k = self.config["CHAT_MODEL_CONFIG"].get("top_k", 40),  
+            )
+
+            pprint(response_data)
+
+            text_output = response_data['choices'][0]['message']['content'].strip()
+        
+        response = parse_llm_output(text_output)
+
+        pprint(response)
+
+        add_messages = [state["query"]] + state["messages"] + state["tools_result"] + [response]
+
+        return {
+            "variables": state["variables"],
+            "system_prompt": state["system_prompt"],
+            "history": add_messages,
+            "branch_name": state["branch_name"],
+            "messages": state["messages"],
+            "tools_result": state["tools_result"],
+            "query": state["query"],
+            "final_answer": response
+        }
 
     # def query_or_respond(self, state: State):
     #     filled_system_prompt = TOOL_PROMPT.format(**state["variables"])
