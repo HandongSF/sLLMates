38e50b8556fd0e8d15be6ccd648c5e7aa5a9429e
joshuaii3712 <joshuaii3712@gmail.com>
2026-03-24 17:46:47 +0900

    Wrapped bio(general) with ToolMessage

--- src/core/agent.py
+++ src/core/agent.py
@@ -822,7 +822,7 @@ class ChatAgent:
             if message.type in ("human") or (message.type == "ai" and not message.tool_calls)
         ]
 
-        trimmed_messages = self.trimmer.invoke([SystemMessage(filled_system_prompt)] + conversation_messages + [state["bio_result"][1]] + [state["query"]])
+        trimmed_messages = self.trimmer.invoke([SystemMessage(filled_system_prompt)] + conversation_messages + [ToolMessage(content=state["bio_result"][1], tool_call_id="temp")] + [state["query"]])
 
         openai_formatted_trimmed_messages = convert_to_openai_messages(trimmed_messages)
 
@@ -1039,7 +1039,7 @@ class ChatAgent:
             if message.type in ("human") or (message.type == "ai" and not message.tool_calls)
         ]
 
-        trimmed_messages = self.trimmer.invoke([SystemMessage(filled_system_prompt)] + conversation_messages + [state["bio_result"][1]] + [state["query"]])
+        trimmed_messages = self.trimmer.invoke([SystemMessage(filled_system_prompt)] + conversation_messages + [ToolMessage(content=state["bio_result"][1], tool_call_id="temp")] + [state["query"]])
 
         openai_formatted_trimmed_messages = convert_to_openai_messages(trimmed_messages)
 
@@ -1151,7 +1151,7 @@ class ChatAgent:
             if message.type in ("human") or (message.type == "ai" and not message.tool_calls)
         ]
 
-        trimmed_messages = self.trimmer.invoke([SystemMessage(filled_system_prompt)] + conversation_messages + state["tools_result"] + [state["bio_result"][1]] + [state["query"]])
+        trimmed_messages = self.trimmer.invoke([SystemMessage(filled_system_prompt)] + conversation_messages + state["tools_result"] + [ToolMessage(content=state["bio_result"][1], tool_call_id="temp")] + [state["query"]])
 
         openai_formatted_trimmed_messages = convert_to_openai_messages(trimmed_messages)
 
@@ -1275,7 +1275,7 @@ class ChatAgent:
             "tools_result": state["tools_result"],
             "bio_result": state["bio_result"],
             "query": state["query"],
-            "final_answer": state["final_answer"]
+            "final_answer": None
         }
     
     # 그래프 생성 함수
